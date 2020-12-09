92065fac5a5b2ba8e9bebd9c09dcdc0273461662
lzervini <lezervinia@gmail.com>
2020-12-09 16:55:57 +0100

    Add new page with exercices

--- page5.php
+++ page5.php
@@ -12,11 +12,33 @@
 <?php
     include 'nav.html';
 ?>
-    <main>
-        <h2> Oups, il semblerait que ça ne soit pas fini par ici ... </h2>
-        <img src="attention_travaux.svg" width :"30%";>
-
-    
+    <main class="cadre">
+    <h1>Répondez à 5 multiplications tirées au hasard !</h1>
+    <form action="" method="post">
+                <?php
+                for ($i = 1; $i <= 5; $i++)
+                {
+                    echo "<div class='separator'><h2>Question n° $i </h2><br>";
+                //On crée deux variables aléatoire
+                    $number1 = random_int(1,10);
+                    $number2= random_int(1,10);
+                    $resultat= $number1*$number2;
+                    echo "$number1 x $number2 = <input type='text' name='reponse$i' class=''><input type='hidden' name='resultat$i' value='$resultat'></div>";
+                    
+                }
+                echo "<br><input type='submit' class='submit' name='verify' value='Je valide !' class='afficher'>";
+                    if(isset($_POST['verify'])){
+                        $score=0;
+                        for($i = 1; $i <= 5; $i++){
+                            if ($_POST["reponse".$i]== $_POST['resultat'.$i]){
+                                $score++;
+                            }
+                        
+                        }
+                        echo "<p>Tu as eu : <span> $score</span> /5</p>";
+                    }
+                ?>
+         </form>    
 </main>
 </body>
 </html>
